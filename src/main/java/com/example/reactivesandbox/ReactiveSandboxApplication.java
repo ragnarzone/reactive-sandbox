40a204dc90b00d637823000bacdd1351e311c697
ragnarzone <ragnarzone@gmail.com>
2023-02-23 09:48:10 +0200

    Initial commit

---
+++ src/main/java/com/example/reactivesandbox/ReactiveSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactivesandbox/ReactiveSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
